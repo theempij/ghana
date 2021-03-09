c3c715e6b8047ba6fb3b8d56151f3f87b3b50d20
Julian <julian.chandra.hauck@gmail.com>
2021-03-09 10:48:06 +0100

    done

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,11 @@
 class ProjectsController < ApplicationController
   def index
+    @title = "Alle Projekte"
+    @projects = Project.all
+  end
 
+  def show
+    @project = Project.find(params[:id])
+    @title = @project.name
   end
 end
