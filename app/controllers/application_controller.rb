f445d5c107016fe211f8a4ecade22b91f9c451c6
Julian <julian.chandra.hauck@gmail.com>
2021-03-13 11:36:59 +0100

    Titles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,26 @@
 class ApplicationController < ActionController::Base
 
   def imprint
-
+    @title = 'Imprint'
   end
 
   def landing_page
-
   end
 
   def news
-
+    @title = 'News'
   end
 
   def donate_now
-
+    @title = 'Donate now!'
   end
 
   def about_ghana
-
+    @title = "About Ghana"
   end
 
   def about_me
-
+    @title = "About me"
   end
 
   def error
